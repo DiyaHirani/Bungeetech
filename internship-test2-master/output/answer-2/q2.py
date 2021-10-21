eb213084374e4cfadbdcf2f652e4115c46db8380
DiyaHirani <61102976+DiyaHirani@users.noreply.github.com>
2021-10-21 13:17:15 +0530

    upload

--- internship-test2-master/output/answer-2/q2.py
+++ internship-test2-master/output/answer-2/q2.py
@@ -4,9 +4,5 @@ population = pd.read_csv("D://bungee tech//internship-test2-master//input//quest
 print(population.head(5))
 a=population.groupby('occupation').min()['age'],population.groupby('occupation').max()['age']
 a=pd.DataFrame(a)
-with open("D://bungee tech//internship-test2-master//output//answer-2//main.csv","a") as file:
-    csvwrite = csv.writer(file)
-    col = ['occupation', str(population.groupby('occupation').min()['age']),str( population.groupby('occupation').max()['age'])]
-    csvwrite.writerow(col)
-
+a.to_csv("D://bungee tech//internship-test2-master//output//answer-2//main.csv")
 print(a)
--- internship-test2-master/output/answer-3/q3.py
+++ internship-test2-master/output/answer-3/q3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('D://bungee tech//internship-test2-master//input//question-3')
+df = pd.read_csv('D://bungee tech//internship-test2-master//input//question-3//main.csv')
 
 yellow = df.sort_values(['Yellow Cards'], ascending=[False])
 
