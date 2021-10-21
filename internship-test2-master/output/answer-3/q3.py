7767b5c5a577f6c29d859616e70b055bc8abe336
DiyaHirani <61102976+DiyaHirani@users.noreply.github.com>
2021-10-21 13:26:30 +0530

    Update q3.py

--- internship-test2-master/output/answer-3/q3.py
+++ internship-test2-master/output/answer-3/q3.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('D://bungee tech//internship-test2-master//input//question-3//main.csv')
 
-yellow = df.sort_values(['Yellow Cards'], ascending=[False])
+y = df.sort_values(['Yellow Cards'], ascending=[False])
 
-red = yellow.sort_values(['Red Cards'], ascending=[False])
+r = y.sort_values(['Red Cards'], ascending=[False])
 
-print(red[['Team', 'Yellow Cards', 'Red Cards']])
+print(r[['Team', 'Yellow Cards', 'Red Cards']])
